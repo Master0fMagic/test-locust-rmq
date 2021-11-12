25c6151ce7809f78989d4910d8675450cbdbddbc
d.tostolug <d.tostolug@simcord.com>
2021-11-12 16:18:29 +0200

    change locust launcher

--- src/locustLauncher.py
+++ src/locustLauncher.py
@@ -32,9 +32,9 @@ def main():
     global env
     env = Environment(user_classes=[RmqUser])
     env.create_local_runner()
-    env.create_web_ui('localhost', 8089)
+    env.create_web_ui('localhost', 8089,)
 
-    gevent.spawn(stats_printer(env.stats))
+    gevent.spawn(stats_printer(env.runner.stats))
     gevent.spawn(stats_history, env.runner)
 
     env.runner.start(1, 1)
