905a8c1eff11abede545dae4b069de7c84765a56
d.tostolug <d.tostolug@simcord.com>
2021-11-12 17:24:34 +0200

    change event firing

--- src/locustfile.py
+++ src/locustfile.py
@@ -5,6 +5,8 @@ from locust import User, task, events, constant
 from contracts import Error
 from error import TimeoutException, ResponseException
 
+REQUEST_TYPE = 'AMQP'
+
 
 class RmqUser(User):
     wait_time = constant(1)
@@ -17,16 +19,28 @@ class RmqUser(User):
 def on_response(response, completion_time):
     if response.Error == Error():
         logging.info(f'Success response: {response}')
-        events.request_success.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
-                                    response_length=0)
+        # events.request_success.fire(response_time=completion_time, request_type='amqp', name='amqp', response_length=0)
+        fire_success(completion_time, response)
     else:
         logging.info(f'Response with error: {response}')
-        events.request_failure.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
-                                    response_length=0, exception=ResponseException())
+        # events.request_failure.fire(response_time=completion_time, request_type='amqp', name='amqp', response_length=0,
+        #                             exception=ResponseException())
+        fire_error(completion_time, response, ResponseException())
     logging.info(f'Response completed in {completion_time} ms')
 
 
 def on_timeout(request_id):
-    events.request_failure.fire(exception=TimeoutException(), response=None, request_type='amqp', name='amqp',
-                                response_length=0)
+    # events.request_failure.fire(exception=TimeoutException(), request_type='amqp', name='amqp', response_length=0,
+    #                             response_time=0)
+    fire_error(0, None, TimeoutException())
     logging.info(f'timeout for request: {request_id}')
+
+
+def fire_success(completion_time, response):
+    events.request.fire(request_type=REQUEST_TYPE, name='Local docker RabbitMQ instance', response_time=completion_time,
+                        response_length=0, response=response, context=None, exception=None)
+
+
+def fire_error(completion_time, response, exception: BaseException):
+    events.request.fire(request_type=REQUEST_TYPE, name='Local docker RabbitMQ instance', response_time=completion_time,
+                        response_length=0, response=response, context=None, exception=BaseException)
