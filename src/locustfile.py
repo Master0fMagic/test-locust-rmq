fda483f518bd6c82db0bfd62264a81b0e3dcd6f8
d.tostolug <d.tostolug@simcord.com>
2021-11-12 10:59:32 +0200

    change request firing

--- src/locustfile.py
+++ src/locustfile.py
@@ -16,16 +16,16 @@ class RmqUser(User):
 def on_response(response, completion_time):
     if response.Error == Error():
         print(f'Success response: {response}')
-        events.request.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
-                            response_length=0, exception=None, context=None)
+        events.request_success.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
+                                    response_length=0)
     else:
         print(f'Response with error: {response}')
-        events.request.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
-                            response_length=0, exception=ResponseException(), context=None)
+        events.request_failure.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
+                                    response_length=0, exception=ResponseException())
     print(f'Response completed in {completion_time} ms')
 
 
 def on_timeout(request_id):
-    events.request.fire(exception=TimeoutException(), response=None, request_type='amqp', name='amqp',
-                        response_length=0, context=None)
+    events.request_failure.fire(exception=TimeoutException(), response=None, request_type='amqp', name='amqp',
+                                response_length=0)
     print(f'timeout for request: {request_id}')
