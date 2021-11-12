2712cb8def3a0bf3314a21756755753fc9849265
d.tostolug <d.tostolug@simcord.com>
2021-11-12 11:25:09 +0200

    remove stats

--- src/locustLauncher.py
+++ src/locustLauncher.py
@@ -30,8 +30,8 @@ def main():
     env.create_local_runner()
     env.create_web_ui('localhost', 8089)
 
-    gevent.spawn(stats_printer(env.stats))
-    gevent.spawn(stats_history, env.runner)
+    # gevent.spawn(stats_printer(env.stats))
+    # gevent.spawn(stats_history, env.runner)
 
     env.runner.start(1, 1)
 
