c4c82ceadb1f2b79200bdc9366e93b2b174dc374
d.tostolug <d.tostolug@simcord.com>
2021-11-12 10:46:47 +0200

    add required arguments, custom errors

--- src/locustfile.py
+++ src/locustfile.py
@@ -1,8 +1,7 @@
 from rabbitClient import get_client
-
 from locust import User, task, events
-
 from contracts import Error
+from error import TimeoutException, ResponseException
 
 
 class RmqUser(User):
@@ -17,13 +16,16 @@ class RmqUser(User):
 def on_response(response, completion_time):
     if response.Error == Error():
         print(f'Success response: {response}')
-        events.request.fire(response_time=completion_time, exception=Exception())
+        events.request.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
+                            response_length=0, exception=None, context=None)
     else:
         print(f'Response with error: {response}')
-        events.request.fire(response_time=completion_time, response=response)
+        events.request.fire(response_time=completion_time, response=response, request_type='amqp', name='amqp',
+                            response_length=0, exception=ResponseException(), context=None)
     print(f'Response completed in {completion_time} ms')
 
 
 def on_timeout(request_id):
-    events.request.fire(exception=Exception())
+    events.request.fire(exception=TimeoutException(), response=None, request_type='amqp', name='amqp',
+                        response_length=0, context=None)
     print(f'timeout for request: {request_id}')
