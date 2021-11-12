5e539484ad6a8b9e7faa698899a2eac1b0b95ef9
d.tostolug <d.tostolug@simcord.com>
2021-11-12 17:26:56 +0200

    remove commented code

--- src/locustfile.py
+++ src/locustfile.py
@@ -19,19 +19,14 @@ class RmqUser(User):
 def on_response(response, completion_time):
     if response.Error == Error():
         logging.info(f'Success response: {response}')
-        # events.request_success.fire(response_time=completion_time, request_type='amqp', name='amqp', response_length=0)
         fire_success(completion_time, response)
     else:
         logging.info(f'Response with error: {response}')
-        # events.request_failure.fire(response_time=completion_time, request_type='amqp', name='amqp', response_length=0,
-        #                             exception=ResponseException())
         fire_error(completion_time, response, ResponseException())
     logging.info(f'Response completed in {completion_time} ms')
 
 
 def on_timeout(request_id):
-    # events.request_failure.fire(exception=TimeoutException(), request_type='amqp', name='amqp', response_length=0,
-    #                             response_time=0)
     fire_error(0, None, TimeoutException())
     logging.info(f'timeout for request: {request_id}')
 
